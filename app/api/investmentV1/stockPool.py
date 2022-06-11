2ca65830e1f794aceee7f7cb17818b9273fa21ba
zhangjiahao <zjh17531008127@163.com>
2022-06-11 19:58:38 +0800

    张嘉豪--修改查询股票池

--- app/api/investmentV1/stockPool.py
+++ app/api/investmentV1/stockPool.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request
 from app.api.investmentV1.model.stockPool import MbaStockPool
-
+from app.api.investmentV1.model.industryClass import MbaIndustryClass
+# end 模型必须引用后才能在数据库初始化对应表
+from lin import db
+from itertools import zip_longest
 StockPool_api = Blueprint("stockPool", __name__)
 
 #查询股票池
@@ -22,9 +25,20 @@ def getStockPoolList():
     if codeName is not None and len(codeName.strip()) > 0:
         filterList.append(MbaStockPool.code_name == codeName)
     # 分页查询
-    dataList = MbaStockPool.query.filter(*filterList) \
-        .order_by(MbaStockPool.code) \
+    pageData = db.session.query(MbaStockPool.code,
+                                MbaStockPool.code_name,
+                                MbaStockPool.periods,
+                                MbaStockPool.create_time,
+                                MbaIndustryClass.industry_sw,
+                                MbaIndustryClass.industry_cit)\
+        .outerjoin(MbaIndustryClass, MbaStockPool.code == MbaIndustryClass.code)\
+        .filter(*filterList).order_by(MbaStockPool.code)\
         .offset(startIndex).limit(pageSize).all()
+    # 定义返回参数列表：顺序和字段名称需要和查询的列保持一致
+    mapList = ['code', 'codeName', 'periods', 'create_time', 'industry_sw', 'industry_cit']
+    dataList = []
+    for returnData in pageData:
+        dataList.append(dict(zip_longest(mapList, returnData)))
     # 获取分页总条数
     totalNum = MbaStockPool.query.filter(*filterList).count()
     # 返回参数信息
